5ae43d0fdfde3d15e6aa3e896a9172a1bd2ffade
Edjek <edjek@hotmail.fr>
2023-04-03 09:52:57 +0200

    algorithmique

--- algorithmique-practice/js/main.js
+++ algorithmique-practice/js/main.js
@@ -44,7 +44,6 @@
 
 // Afficher le message avec les 3 facons de faire une concaténation
 
-
 // ---------------------------------- Partie 2 ----------------------------------
 
 // Creer une fonction qui affiche un message de bienvenue dans la console
@@ -58,7 +57,6 @@
 // Creer une fonction qui affiche un message de bienvenue dans la console et qui prend en parametre un nom et un age
 // 'Bonjour XXXX, vous avez XXXX ans, je suis une fonction qui affiche un message de bienvenue dans la console et qui prend deux parametres'
 // Appeler la fonction en lui passant votre prenom et votre age en parametre
-
 
 // ---------------------------------- Partie 3 ----------------------------------
 
@@ -80,7 +78,6 @@
 // Transformer l'exercice en fonction qui prend en parametre un animal et qui retourne le message correspondant
 
 // Utiliser la methode switch pour faire la meme chose
-
 
 // ---------------------------------- Partie 4 ----------------------------------
 
@@ -111,7 +108,6 @@
 // Et qui affiche dans la console si le nombre est pair ou impair (modulo)
 // Appeler la fonction en lui passant le nombre 5 en parametre
 // Appeler la fonction en lui passant le nombre 256 en parametre
-
 
 // ---------------------------------- Partie 5 ----------------------------------
 
@@ -190,7 +186,6 @@
 // Recuperer la mention de l'utilisateur
 // Afficher dans la console le nom de l'utilisateur, sa moyenne, s'il est reçu ou non et sa mention
 
-
 // ---------------------------------- Partie 6 ----------------------------------
 
 // Creer un objet voiture
@@ -238,65 +233,71 @@
 // Voici un tableau qui contient 6 objets utilisateurs
 let users = [
     {
-        prenom: "Alice",
-        nom: "Dupont",
+        prenom: 'Alice',
+        nom: 'Dupont',
         age: 28,
-        sexe: "Femme",
-        animauxPreferes: ["chat", "chien"],
+        sexe: 'Femme',
+        animauxPreferes: ['chat', 'chien'],
         description: function () {
-            return `Je m'appelle ${this.prenom} ${this.nom} et j'ai ${this.age} ans et j'aime les ${this.animauxPreferes.join(", ")}.`;
-        }
+            return `Je m'appelle ${this.prenom} ${this.nom} et j'ai ${this.age
+                } ans et j'aime les ${this.animauxPreferes.join(', ')}.`;
+        },
     },
     {
-        prenom: "Bob",
-        nom: "Martin",
+        prenom: 'Bob',
+        nom: 'Martin',
         age: 35,
-        sexe: "Homme",
-        animauxPreferes: ["poisson", "oiseau"],
+        sexe: 'Homme',
+        animauxPreferes: ['poisson', 'oiseau'],
         description: function () {
-            return `Je m'appelle ${this.prenom} ${this.nom} et j'ai ${this.age} ans et j'aime les ${this.animauxPreferes.join(", ")}.`;
-        }
+            return `Je m'appelle ${this.prenom} ${this.nom} et j'ai ${this.age
+                } ans et j'aime les ${this.animauxPreferes.join(', ')}.`;
+        },
     },
     {
-        prenom: "Claire",
-        nom: "Rousseau",
+        prenom: 'Claire',
+        nom: 'Rousseau',
         age: 42,
-        sexe: "Femme",
-        animauxPreferes: ["cheval", "lapin"],
+        sexe: 'Femme',
+        animauxPreferes: ['cheval', 'lapin'],
         description: function () {
-            return `Je m'appelle ${this.prenom} ${this.nom} et j'ai ${this.age} ans et j'aime les ${this.animauxPreferes.join(", ")}.`;
-        }
+            return `Je m'appelle ${this.prenom} ${this.nom} et j'ai ${this.age
+                } ans et j'aime les ${this.animauxPreferes.join(', ')}.`;
+        },
     },
     {
-        prenom: "David",
-        nom: "Lefebvre",
+        prenom: 'David',
+        nom: 'Lefebvre',
         age: 22,
-        sexe: "Homme",
-        animauxPreferes: ["chat", "chien"],
+        sexe: 'Homme',
+        animauxPreferes: ['chat', 'chien'],
         description: function () {
-            return `Je m'appelle ${this.prenom} ${this.nom} et j'ai ${this.age} ans et j'aime les ${this.animauxPreferes.join(", ")}.`;
-        }
+            return `Je m'appelle ${this.prenom} ${this.nom} et j'ai ${this.age
+                } ans et j'aime les ${this.animauxPreferes.join(', ')}.`;
+        },
     },
     {
-        prenom: "Emma",
-        nom: "Garcia",
+        prenom: 'Emma',
+        nom: 'Garcia',
         age: 29,
-        sexe: "Femme",
-        animauxPreferes: ["chien", "hamster"],
+        sexe: 'Femme',
+        animauxPreferes: ['chien', 'hamster'],
         description: function () {
-            return `Je m'appelle ${this.prenom} ${this.nom} et j'ai ${this.age} ans et j'aime les ${this.animauxPreferes.join(", ")}.`;
-        }
+            return `Je m'appelle ${this.prenom} ${this.nom} et j'ai ${this.age
+                } ans et j'aime les ${this.animauxPreferes.join(', ')}.`;
+        },
     },
     {
-        prenom: "François",
-        nom: "Moreau",
+        prenom: 'François',
+        nom: 'Moreau',
         age: 46,
-        sexe: "Homme",
-        animauxPreferes: ["chat", "oiseau"],
+        sexe: 'Homme',
+        animauxPreferes: ['chat', 'oiseau'],
         description: function () {
-            return `Je m'appelle ${this.prenom} ${this.nom} et j'ai ${this.age} ans et j'aime les ${this.animauxPreferes.join(", ")}.`;
-        }
-    }
+            return `Je m'appelle ${this.prenom} ${this.nom} et j'ai ${this.age
+                } ans et j'aime les ${this.animauxPreferes.join(', ')}.`;
+        },
+    },
 ];
 
 // Faire une boucle sur le tableau users
@@ -309,25 +310,99 @@ let users = [
 // Choisissez la méthode qui vous semble la plus adaptée pour itérer sur le tableau users
 // Boucler sur chaque utilisateur pour afficher toutes les informations de l'utilisateur
 
-
 // ---------------------------------- Partie 7 ----------------------------------
 
 const students = [
-    { nom: "Dupont", prenom: "Alice", cours: ["Maths", "Anglais", "Histoire"], notes: [14, 16, 18] },
-    { nom: "Martin", prenom: "Bob", cours: ["Physique", "Espagnol", "Géographie"], notes: [12, 13, 15] },
-    { nom: "Lefebvre", prenom: "Claire", cours: ["Chimie", "Allemand", "SVT"], notes: [9, 10, 11] },
-    { nom: "Girard", prenom: "David", cours: ["Français", "Arts plastiques", "Philosophie"], notes: [17, 19, 14] },
-    { nom: "Petit", prenom: "Emma", cours: ["Latin", "Musique", "Sciences économiques"], notes: [16, 11, 12] },
-    { nom: "Moreau", prenom: "François", cours: ["Sport", "Théâtre", "Informatique"], notes: [11, 13, 15] },
-    { nom: "Dubois", prenom: "Marc", cours: ["Géométrie", "Latin", "Marketing"], notes: [15, 12, 10] },
-    { nom: "Garcia", prenom: "Hugo", cours: ["Littérature", "Musique", "Droit"], notes: [14, 17, 18] },
-    { nom: "Roux", prenom: "Inès", cours: ["Sociologie", "Japonais", "Chimie"], notes: [10, 9, 12] },
-    { nom: "Berger", prenom: "Jules", cours: ["Géographie", "Théâtre", "Espagnol"], notes: [13, 16, 14] },
-    { nom: "Moulin", prenom: "Karine", cours: ["Histoire de l'art", "Anglais", "Physique"], notes: [17, 15, 18] },
-    { nom: "David", prenom: "Lucie", cours: ["Chinois", "Marketing", "SVT"], notes: [12, 11, 9] },
-    { nom: "Mercier", prenom: "Marc", cours: ["Philosophie", "Sport", "Français"], notes: [18, 13, 16] },
-    { nom: "Fournier", prenom: "Nina", cours: ["Informatique", "Littérature", "Musique"], notes: [15, 14, 17] },
-    { nom: "Morel", prenom: "Olivier", cours: ["Espagnol", "Chimie", "Théâtre"], notes: [13, 11, 16] }
+    {
+        nom: 'Dupont',
+        prenom: 'Alice',
+        cours: ['Maths', 'Anglais', 'Histoire'],
+        notes: [14, 16, 18],
+    },
+    {
+        nom: 'Martin',
+        prenom: 'Bob',
+        cours: ['Physique', 'Espagnol', 'Géographie'],
+        notes: [12, 13, 15],
+    },
+    {
+        nom: 'Lefebvre',
+        prenom: 'Claire',
+        cours: ['Chimie', 'Allemand', 'SVT'],
+        notes: [9, 10, 11],
+    },
+    {
+        nom: 'Girard',
+        prenom: 'David',
+        cours: ['Français', 'Arts plastiques', 'Philosophie'],
+        notes: [17, 19, 14],
+    },
+    {
+        nom: 'Petit',
+        prenom: 'Emma',
+        cours: ['Latin', 'Musique', 'Sciences économiques'],
+        notes: [16, 11, 12],
+    },
+    {
+        nom: 'Moreau',
+        prenom: 'François',
+        cours: ['Sport', 'Théâtre', 'Informatique'],
+        notes: [11, 13, 15],
+    },
+    {
+        nom: 'Dubois',
+        prenom: 'Marc',
+        cours: ['Géométrie', 'Latin', 'Marketing'],
+        notes: [15, 12, 10],
+    },
+    {
+        nom: 'Garcia',
+        prenom: 'Hugo',
+        cours: ['Littérature', 'Musique', 'Droit'],
+        notes: [14, 17, 18],
+    },
+    {
+        nom: 'Roux',
+        prenom: 'Inès',
+        cours: ['Sociologie', 'Japonais', 'Chimie'],
+        notes: [10, 9, 12],
+    },
+    {
+        nom: 'Berger',
+        prenom: 'Jules',
+        cours: ['Géographie', 'Théâtre', 'Espagnol'],
+        notes: [13, 16, 14],
+    },
+    {
+        nom: 'Moulin',
+        prenom: 'Karine',
+        cours: ["Histoire de l'art", 'Anglais', 'Physique'],
+        notes: [17, 15, 18],
+    },
+    {
+        nom: 'David',
+        prenom: 'Lucie',
+        cours: ['Chinois', 'Marketing', 'SVT'],
+        notes: [12, 11, 9],
+    },
+    {
+        nom: 'Mercier',
+        prenom: 'Marc',
+        cours: ['Philosophie', 'Sport', 'Français'],
+        notes: [18, 13, 16],
+    },
+    {
+        nom: 'Fournier',
+        prenom: 'Nina',
+        cours: ['Informatique', 'Littérature', 'Musique'],
+        notes: [15, 14, 17],
+    },
+    {
+        nom: 'Morel',
+        prenom: 'Olivier',
+        cours: ['Espagnol', 'Chimie', 'Théâtre'],
+        notes: [13, 11, 16],
+    },
 ];
 
 // Itérer sur le tableau students avec la méthode forEach()
@@ -340,7 +415,7 @@ const students = [
 
 // 4. Afficher dans la console le nom et la moyenne de chaque élève
 
-// 5. Afficher dans la console le nom et la moyenne de chaque élève 
+// 5. Afficher dans la console le nom et la moyenne de chaque élève
 // si la moyenne est supérieure ou égale à 15
 // Afficher dans la console 'vous obtenez une bourse'
 // Si la moyenne estsupérieure ou égale à 10
@@ -349,4 +424,3 @@ const students = [
 // Afficher dans la console 'vous allez en rattrapage'
 // Si la moyenne est inférieure à 9
 // Afficher dans la console 'vous redoublez'
-
