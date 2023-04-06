73087a9e951bc462b7a75617747ff76eb2fb33d1
Edjek <edjek@hotmail.fr>
2023-04-06 22:04:58 +0200

    js

--- javascript/js/lesson/03-concatenation.js
+++ javascript/js/lesson/03-concatenation.js
@@ -8,6 +8,8 @@ console.log(`J'ai ${age} ans`);
 
 let lastName  = 'Rachid';
 let firstName = 'EDJEKOUANE';
+
+// 3 manières de concaténer du texte et des variables
 console.log("Bonjour, je m'appelle " + firstName + ' ' + lastName);
 console.log("Bonjour, je m'appelle " + firstName + ' ' + lastName);
 console.log(`Bonjour, je m'appelle ${firstName} ${lastName}`);
