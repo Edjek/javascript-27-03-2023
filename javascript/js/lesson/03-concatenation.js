627bfb34ab362bb5f7cc5b522bcb8e0c5d56d59f
Edjek <edjek@hotmail.fr>
2023-04-06 22:06:13 +0200

    js

--- javascript/js/lesson/03-concatenation.js
+++ javascript/js/lesson/03-concatenation.js
@@ -3,10 +3,10 @@
 // Concaténation (Afficher du texte et des variables)
 let age = 18;
 console.log("J'ai " + age + ' ans');
-console.log("J'ai " + age + ' ans');
+console.log('J\'ai ' + age + ' ans');
 console.log(`J'ai ${age} ans`);
 
-let lastName  = 'Rachid';
+let lastName = 'Rachid';
 let firstName = 'EDJEKOUANE';
 
 // 3 manières de concaténer du texte et des variables
